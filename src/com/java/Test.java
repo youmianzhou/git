17fb1f26ec7b0690fcdee410d7210094d7b6beb4
youmianzhou <youmianzhou@126.com>
2020-12-05 21:23:24 +0800

    第2次提交

--- src/com/java/Test.java
+++ src/com/java/Test.java
@@ -4,5 +4,6 @@ package com.java;
 public class Test {
     public static void main(String[] args) {
         System.out.println("Hello World!!!");
+        System.out.println("Hello World!!!");
     }
 }
